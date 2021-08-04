fe9ddef7ed6467e46783ecc6242b835097bca24f
MARCOS JAMPIETRI <marcosjampietri@marcoss-macbook-pro.local>
2021-08-04 21:14:23 +0000

    typescript setup

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,11 +1,20 @@
 import Head from "next/head";
 import Image from "next/image";
+
+import styled from "styled-components";
+import { animated } from "react-spring";
+
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <div>Rélou Github hook</div>
-    </div>
+    <Page>
+      <div>Rélou Taips quíprit</div>
+    </Page>
   );
 }
+
+const Page = styled(animated.div)`
+  width: 100%;
+  color: hsla(327, 100%, 19%, 1);
+`;
