ac0fe70edce7ba986ce486a91f52a048ffdf4a1d
MARCOS JAMPIETRI <marcosjampietri@marcoss-macbook-pro.local>
2021-08-06 22:38:55 +0000

    declare styled components

--- client/next-env.d.ts
+++ client/next-env.d.ts
@@ -1,3 +1,12 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    colors: {
+      primary: string;
+    };
+  }
+}
