e29f96a474ed0f2499465bae799145eb07a6c482
MARCOS JAMPIETRI <marcosjampietri@marcoss-macbook-pro.local>
2021-08-09 20:54:47 +0000

    rm volumes from docker compose client

--- client/next-env.d.ts
+++ client/next-env.d.ts
@@ -1,12 +1,3 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
-import "styled-components";
-
-declare module "styled-components" {
-  export interface DefaultTheme {
-    colors: {
-      primary: string;
-    };
-  }
-}
