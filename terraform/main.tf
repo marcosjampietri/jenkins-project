83c990fb05bf5e70b062e87d9a2e2e5c96376d23
MARCOS JAMPIETRI <marcosjampietri@marcoss-macbook-pro.local>
2021-08-09 21:03:59 +0000

    add inbound rules

--- terraform/main.tf
+++ terraform/main.tf
@@ -81,6 +81,27 @@ resource "aws_security_group" "marcos-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
