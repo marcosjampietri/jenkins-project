0f6c356148a6b2404a57279111939a816b0701d3
MARCOS JAMPIETRI <marcosjampietri@marcoss-macbook-pro.local>
2021-09-29 22:15:08 +0000

    fix type errors

--- client/pages/outra.tsx
+++ client/pages/outra.tsx
@@ -1,41 +1,40 @@
 import Link from "next/link";
-import { useSelector } from "react-redux";
 
 import styled from "styled-components";
 import { animated, useSpring, config } from "react-spring";
 
-import { AppState } from "./_app";
+import { AppState, useTypedSelector } from "../store/reducers/rootReducer";
 
 export default function Outra() {
-  const { NavOn } = useSelector((state: AppState) => state.nav);
+    const { NavOn } = useTypedSelector((state: AppState) => state.nav);
 
-  const textSwipe = useSpring({
-    config: config.slow,
-    delay: 800,
-    transform: NavOn ? "translate3d(100vw,0,0)" : "translate3d(100vw,0,0)",
-  });
+    const textSwipe = useSpring({
+        config: config.slow,
+        delay: 800,
+        transform: NavOn ? "translate3d(100vw,0,0)" : "translate3d(100vw,0,0)",
+    });
 
-  return (
-    <Page>
-      <div>🥳</div>
-      <div>IM RUNNING FROM A FULL DEVOPS PROCESS YAYYYY!!!</div>
-      <Link href="/">
-        <Text style={textSwipe}>SHUT UP!!</Text>
-      </Link>
-    </Page>
-  );
+    return (
+        <Page>
+            <div>🥳</div>
+            <div>IM RUNNING FROM A FULL DEVOPS PROCESS YAYYYY!!!</div>
+            <Link href="/">
+                <Text style={textSwipe}>SHUT UP!!</Text>
+            </Link>
+        </Page>
+    );
 }
 
 const Page = styled(animated.div)`
-  width: 100%;
-  color: hsla(327, 100%, 19%, 1);
-  position: relative;
+    width: 100%;
+    color: hsla(327, 100%, 19%, 1);
+    position: relative;
 `;
 
 const Text = styled(animated.a)`
-  width: 100px;
-  height: 100px;
+    width: 100px;
+    height: 100px;
 
-  color: hsla(327, 100%, 19%, 1);
-  cursor: pointer;
+    color: hsla(327, 100%, 19%, 1);
+    cursor: pointer;
 `;
